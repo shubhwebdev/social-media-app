80112b9dc58fe05c8ea17f92d842be01681c894a
Shubham PHP <shubham@GRAZITTI.LOCAL>
2022-09-13 15:43:27 +0530

    updates for signup form - added axios, made a basic form submission, some minor fixes

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,124 +1,76 @@
-import { useState } from "react"
-
-import Logo from './../../assets/images/logo.svg'
-
-
-import {AppBar,Box,Toolbar,Typography,Container,IconButton,Menu,MenuItem,Button} from '@mui/material';
-
-import MenuIcon from '@mui/icons-material/Menu';
-
-const pages = ['Sign in'];
+import Logo from "./../../assets/images/logo.svg";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+  Button,
+} from "@mui/material";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
 
-  const brandName = 'Moments';
-  
-  const [anchorElNav, setAnchorElNav] = useState(null);
+  const currPath = location.pathname;
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  const headerBtnContent =
+    currPath === "/signup"
+      ? { path: "signin", title: "Sign In" }
+      : { path: "signup", title: "Sign Up" };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  const headerBtn = (
+    <Button
+      component={Link}
+      key={headerBtnContent.path}
+      to={`/${headerBtnContent.path}`}
+      variant="contained"
+      sx={{
+        display: "block",
+        textTransform: "capitalize",
+        ml: "auto",
+        border: "2px solid black",
+      }}
+    >
+      {headerBtnContent.title}
+    </Button>
+  );
 
+  const brandName = "Moments";
 
   return (
-    <AppBar sx={{backgroundColor: 'white', boxShadow:'none'}} position="static">
-    <Container>
-      <Toolbar disableGutters>
-        <img src={Logo} alt="" />
-        <Typography
-          variant="h6"
-          noWrap
-          component="a"
-          href="/"
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: ['Rubik','sans-serif'].join(','),
-            fontWeight: 700,
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          {brandName.toUpperCase()}
-        </Typography>
-
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-            color="inherit"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
+    <AppBar
+      sx={{ backgroundColor: "white", boxShadow: "none" }}
+      position="static"
+    >
+      <Container>
+        <Toolbar disableGutters>
+          <img src={Logo} alt="" />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
             sx={{
-              display: { xs: 'block', md: 'none' },
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            {pages.map((page) => (
-              <MenuItem key={page} onClick={handleCloseNavMenu}>
-                <Typography textAlign="center">{page}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-        
-        <Typography
-          variant="h5"
-          noWrap
-          component="a"
-          href=""
-          sx={{
-            mr: 2,
-            display: { xs: 'flex', md: 'none' },
-            flexGrow: 1,
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          {brandName.toUpperCase()}
-        </Typography>
-        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          {pages.map((page) => (
-            <Button
-              key={page}
-              onClick={handleCloseNavMenu}
-              variant='contained'
-              sx={{display: 'block',textTransform:'capitalize',ml: 'auto',border:'2px solid black' }}
-            >
-              {page}
-            </Button>
-          ))}
-        </Box>
+            {brandName.toUpperCase()}
+          </Typography>
 
-      </Toolbar>
-    </Container>
-  </AppBar>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {headerBtn}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-}
+};
 
-
-export default Header
+export default Header;
--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
-import SignIn from './../pages/SignIn'
-import SignUp from './../pages/SignUp'
+import Auth from '../pages/Auth'
 
 const site_routes = props => {
   return (
     <React.Fragment>
       <Routes>
-          <Route path="/signin" element={<SignIn/>}></Route>
-          <Route path="/signup" element={<SignUp/>}></Route>
-          <Route path="/" element={<SignIn/>}></Route>
+          <Route path="/signin" element={<Auth page='signin'/>}></Route>
+          <Route path="/signup" element={<Auth page='signup'/>}></Route>
+          <Route path="/" element={<Auth page='signin'/>}></Route>
       </Routes>
     </React.Fragment>
   )
